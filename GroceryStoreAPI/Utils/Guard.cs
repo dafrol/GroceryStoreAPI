5b73452e9429dff5fa729b551899a5d05e3c8f5d
dafrol <dfroehli@gmail.com>
2021-04-19 13:33:41 -0400

    Add files via upload

---
+++ GroceryStoreAPI/Utils/Guard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GroceryStoreAPI.Utils
+{
+    public static class Guard
+    {
+        public static void VerifyArgumentNotNull(object obj, string objName)
+        {
+            if (obj == null)
+            {
+                Exception ex = new ArgumentNullException(objName);
+                throw (ex);
+            }
+        }
+
+    }
+}
