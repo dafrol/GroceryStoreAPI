abb2de36374ff7a85e4370348f2624afa2c9ce9f
dafrol <dfroehli@gmail.com>
2021-04-19 13:26:28 -0400

    Add files via upload

---
+++ GroceryStoreAPI.Tests/Controllers/CustomerControllerTests.cs
@@ -0,0 +1,97 @@
+﻿using GroceryStoreAPI.Controllers;
+using GroceryStoreAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace GroceryStoreAPI.Tests.Controllers
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        private Customer _customerToAdd = new Customer() { Id = 3, Name = "Dan" };
+        private Customer _customerToUpdate = new Customer() { Id = 2, Name = "Bill" };
+        private Dictionary<int, Customer> _customers = new Dictionary<int, Customer>();
+
+        #region Constructor
+        [Test]
+        public void Constructor_Fails_With_Invalid_Repository()
+        {
+            Assert.That(() => new CustomerController(null), Throws.ArgumentNullException);
+        }
+        [Test]
+        public void Constructor_Is_Successful_With_Valid_Repository()
+        {
+            Assert.That(() => new CustomerController(Mock.Of<ICustomerRepository>()), Throws.Nothing);
+        }
+        #endregion
+
+
+        [Test]
+        public void GetCustomers_ReturnsAllCustomers()
+        {
+            Mock<ICustomerRepository> mockRepository = _getMockCustomers();
+            CustomerController customerController = new CustomerController(mockRepository.Object);
+
+            IEnumerable<Customer> result = customerController.GetCustomers();
+            Assert.That(mockRepository.Object.Customers == result);
+        }
+
+        [Test]
+        public void GetCustomer_ReturnsOK()
+        {
+            Mock<ICustomerRepository> mockRepository = _getMockCustomers();
+            CustomerController customerController = new CustomerController(mockRepository.Object);
+            
+            Microsoft.AspNetCore.Mvc.ActionResult result = customerController.GetCustomer(1).Result;
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [Test]
+        public void GetInvalidCustomer_ReturnsBadRequest()
+        {
+            Mock<ICustomerRepository> mockRepository = _getMockCustomers();
+            CustomerController customerController = new CustomerController(mockRepository.Object);
+
+            Microsoft.AspNetCore.Mvc.ActionResult result = customerController.GetCustomer(5).Result;
+            Assert.IsInstanceOf<BadRequestObjectResult>(result);
+        }
+
+        [Test]
+        public void AddCustomer_ReturnsAddedCustomer()
+        {
+            Mock<ICustomerRepository> mockRepository = _getMockCustomers();
+            CustomerController customerController = new CustomerController(mockRepository.Object);
+
+            Customer result = customerController.Post(_customerToAdd);
+            Assert.That(result == _customerToAdd);
+        }
+
+        [Test]
+        public void UpdateCustomer_ReturnsUpdatedCustomer()
+        {
+            Mock<ICustomerRepository> mockRepository = _getMockCustomers();
+            CustomerController customerController = new CustomerController(mockRepository.Object);
+
+            Customer result = customerController.Put(_customerToUpdate);
+            Assert.That(result == _customerToUpdate);
+        }
+
+
+        private Mock<ICustomerRepository> _getMockCustomers()
+        {
+            _customers = new Dictionary<int, Customer>();
+            _customers.Add(0, new Customer { Id = 1, Name = "Bob" });
+            _customers.Add(1, new Customer { Id = 2, Name = "Mary" });
+            _customers.Add(2, new Customer { Id = 3, Name = "Joe" });
+
+            Mock<ICustomerRepository> mock = new Mock<ICustomerRepository>();
+            mock.Setup(m => m.Customers).Returns(_customers.Values);
+            mock.Setup(m => m.AddCustomer(It.IsAny<Customer>())).Returns(_customerToAdd);
+            mock.Setup(m => m.UpdateCustomer(It.IsAny<Customer>())).Returns(_customerToUpdate);
+
+            return mock;
+        }
+    }
+}
---
+++ GroceryStoreAPI.Tests/Model/CustomerRepositoryTests.cs
@@ -0,0 +1,57 @@
+﻿using GroceryStoreAPI.Models;
+using NUnit.Framework;
+
+namespace GroceryStoreAPI.Tests.Model
+{
+    [TestFixture]
+    public class CustomerRepositoryTests
+    {
+        [Test]
+        public void AddCustomerFails_With_Invalid_Data()
+        {
+            CustomerRepository repo = new CustomerRepository();
+            
+            Customer customerNoName = new Customer() { Id = 1, Name = "" };
+            Customer customerInvalidId = new Customer() { Id = -1, Name = "Fred" };
+            
+            Customer noNameResult = repo.AddCustomer(customerNoName);
+            Customer invalidIdResult = repo.AddCustomer(customerInvalidId);
+            
+            Assert.That(noNameResult == null);
+            Assert.That(invalidIdResult == null);
+        }
+
+        [Test]
+        public void AddCustomerIsSuccessful_With_Valid_Data()
+        {
+            CustomerRepository repo = new CustomerRepository();
+
+            Customer newCustomer = new Customer() { Id = 1, Name = "Fred" };
+            Customer result = repo.AddCustomer(newCustomer);
+
+            Assert.That(result == newCustomer);
+        }
+
+        [Test]
+        public void UpdateCustomerFails_With_Unknown_Customer_Id()
+        {
+            CustomerRepository repo = new CustomerRepository();
+
+            Customer customerToUpdate = new Customer() { Id = 999, Name = "Sharon" };
+            Customer result = repo.UpdateCustomer(customerToUpdate);
+
+            Assert.That(result == null);
+        }
+
+        [Test]
+        public void UpdateCustomerIsSuccessful_With_Valid_Customer_Id()
+        {
+            CustomerRepository repo = new CustomerRepository();
+
+            Customer customerToUpdate = new Customer() { Id = 2, Name = "Sharon" };
+            Customer result = repo.UpdateCustomer(customerToUpdate);
+
+            Assert.That(result.Id == customerToUpdate.Id && result.Name == customerToUpdate.Name);
+        }
+    }
+}
